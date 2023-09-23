36cec3338fa81467081bf82459c515878acb3c01
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2023-09-23 18:25:54 +0700

    react router setup and using links

---
+++ src/pages/Contact.js
@@ -0,0 +1,19 @@
+function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend
+        quam vel mi iaculis porta. Donec eu iaculis elit. Aliquam eget sapien
+        tellus. Aenean quis lacus ac ante volutpat semper. Vestibulum fringilla
+        vitae ipsum ut dictum. Integer eget dignissim diam. Morbi at consequat
+        eros. Quisque eu dolor mauris. Pellentesque condimentum dui id augue
+        ornare convallis. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Suspendisse suscipit ultricies elit, ut pulvinar neque facilisis
+        ut.
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
